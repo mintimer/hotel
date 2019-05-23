9d41653a481f2361f6ba11c2d668dfe97c83efc6
aicquewk <vuttithatkrongyot@gmail.com>
2019-05-23 21:14:10 +0700

    login Toommmm

---
+++ login.php
@@ -0,0 +1,42 @@
+<html lang="en">
+    <head>
+            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">        
+                        <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+            <meta charset="utf-8">
+      <link rel="stylesheet" href="fangstyle.css">
+        <title>login</title>
+    </head>
+
+    <body>
+            <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #339999;" id="mynav">
+                    <a class="navbar-brand" href="#" style="color: white">
+                        <img src="pic/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                        JustFang
+                    </a>
+            </nav>
+            <div class="card" style="width: Responsive;">
+                    <img class="card-img-top" src="pic/hotel.jpeg" alt="Card image cap">
+                    <div class="card-body">
+                            <form action="welcome.php" method="post" >
+                                    <div class="form-group">
+                                      <label for="inputusername">Username</label>
+                                      <input type="username" name="user" class="form-control" id="exampleInputUsername" aria-describedby="usernameHelp" placeholder="Enter username">  
+                                    </div>
+
+                                    <div class="form-group">
+                                      <label for="InputPassword">Password</label>
+                                      <input type="password" name="pass" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                    </div>
+                                    <button type="submit" name="btn" value="member" class="btn btn-outline-primary" aria-pressed="true">LOG IN</button>
+                                    <button type="submit" name="btn" value="GUEST" class="btn btn-outline-secondary" aria-pressed="true">GUEST</button>
+                            
+                                    
+                                  </form>
+                    </div>
+            </div>
+    </body>
+</html>
