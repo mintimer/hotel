647b9269e0eeefce935ea04c08c1dbb841b129bd
aicquewk <vuttithatkrongyot@gmail.com>
2019-05-23 19:16:41 +0700

    re url to purgati

--- connect.php
+++ connect.php
@@ -1,5 +1,6 @@
 <?php
-    $con=mysqli_connect("127.0.0.1","root","","hotel");
+    $url="purgati.serveo.net:3307";
+    $con=mysqli_connect($url,"root","","hotel");
     if(mysqli_connect_errno()){
         echo "Failed to connect to MySQL:".mysqli_connect_error();
     }else echo "Connect Success";
