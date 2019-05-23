620eb0e1e781ad377648fd51b3a27be6a0d1af7a
mintimer <mintmafiacake@gmail.com>
2019-05-23 18:58:09 +0700

    new userhome

--- userhome.php
+++ userhome.php
@@ -3,27 +3,25 @@
 <title>Home After login</title>
 </head>
 <body>
-        <!-- // $con=mysqli_connect("vesper.serveo.net","root","","hotel");
-        // if(mysqli_connect_errno()){
-        //     echo "Failed to connect to MySQL:".mysqli_connect_error();
-        // }else echo "Connect Success<br>";
-
-        // $username = mysqli_real_escape_string($con,$_POST['user']);
-        // $password = mysqli_real_escape_string($con,$_POST['pass']);
-
-        // $result = mysqli_query($con,"SELECT * FROM memberinfo 
-        //                         WHERE Username = '$username' and 
-        //                         Password = '$password' ")
-        //         or die("Failed to query database ".mysql_error());
-
-        // $row = mysqli_fetch_array($result);
-        // if($row['Username'] == $username && $row['Password'] == $password){
-        //     // $name = mysqli_query($con,"SELECT firstname FROM memberinfo WHERE Username = '$username'");
-        //     // echo "Login success <br> Welcome ".$name;
-        // }else{
-        //     echo "Failed to login.";
-        // // }
-        // // mysqli_close($con); -->
+    <?php
+        include('connect.php');
+        $username = mysqli_real_escape_string($con,$_POST['user']);
+        $password = mysqli_real_escape_string($con,$_POST['pass']);
+        $result = mysqli_query($con,"SELECT * FROM memberinfo 
+                                WHERE Username = '$username' and 
+                                Password = '$password' ")
+                or die("Failed to query database ".mysql_error());
+        $row = mysqli_fetch_array($result);
+        if($row['Username'] == $username && $row['Password'] == $password){
+            $name = mysqli_query($con,"SELECT firstname,lastname,userid FROM memberinfo WHERE Username = '$username'");
+            $namee = mysqli_fetch_array($name);
+            echo "Login success <br> Welcome ".$row['Username'];
+            
+        }else{
+            echo "Failed to login.";
+        }
+        
+        ?>
   
     <!-- <style>
             #book {
@@ -53,18 +51,20 @@
                 background: rgb(5, 214, 5);
             }
     </style> -->
+    
     <form action="booking_form.php" method="POST">
-            <input type="hidden" name="u_firstname" value="dummyfirstname">
-            <input type="hidden" name="u_lastname" value="dummylastname">
-            <input type="hidden" name="u_id" value="dummyID">
+            <input type="hidden" name="u_firstname" value=<?php echo $namee['firstname'];?>>
+            <input type="hidden" name="u_lastname" value=<?php echo $namee['lastname'];?>>
+            <input type="hidden" name="u_id" value=<?php echo $namee['userid'];?>>
             <input type="submit" value="Booking">
     </form>
     <form action="review_form.php" method="POST">
-            <input type="hidden" name="u_firstname" value="dummyfirstname">
-            <input type="hidden" name="u_lastname" value="dummylastname">
-            <input type="hidden" name="u_id" value="dummyID">
+            <input type="hidden" name="u_firstname" value=<?php echo $namee['firstname'];?>>
+            <input type="hidden" name="u_lastname" value=<?php echo $namee['lastname'];?>>
+            <input type="hidden" name="u_id" value=<?php echo $namee['userid'];?>>
             <input type="submit" value="Review">
     </form>
-
+        
+    <?php mysqli_close($con); ?>
 </body>
 </html>
