c4e0893cd395e4ac2a298b741172db32b2a32ed2
manmanandman <prakasit_sw2@outlook.com>
2019-05-26 01:39:32 +0700

    ss

--- formregisroom.php
+++ formregisroom.php
@@ -8,16 +8,28 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <?php include('connect.php');
     ?>
+    <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+
+        th {
+            text-align: center;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
         <h3>Room Registration</h3>
-        ฟอร์มเพิ่มห้องพัก <br><br>
+        ฟอร์มเพิ่มห้องพัก Staff ID : <br><br>
 
         <form action="#" method="get">
             <label for="validationHotel">Select Hotel</label>
-            <select name="branch" id="validationHotel" class="custom-select" required>
+            <select name="branch" id="validationHotel" class="custom-select is-invalid" required onchange="hotelvalidate()">
                 <option selected value="">Hotel</option>
                 <?php
                 $sql = "SELECT branchname,branchno FROM branchinfo";
@@ -31,33 +43,38 @@
 
             <div class="form-row">
                 <div class="col">
-                    <label for="validationRoomNumber">Room Number</label>
-                    <input type="text" id="validationRoomNumber" class="form-control" placeholder="Enter Room Number" name=roomnumber required>
+                    <label for="validationRoomNumber">Room Number (3 Digits Number)</label>
+                    <input type="text" id="validationRoomNumber" class="form-control is-invalid" onchange="roomnumbervalidate()" placeholder="Enter Room Number" name="roomnumber" input pattern="[0-9]{3}" minlength="3" maxlength="3" size="3" required>
                 </div>
                 <div class="col">
                     <label for="validationRoomType">Room Type</label>
-                    <select name="roomtype" id="validationRoomType" class="custom-select" required>
+                    <select name="roomtype" id="validationRoomType" class="custom-select is-invalid" required onchange="roomtypevalidate()">
                         <option selected value="">Choose Room Type</option>
                         <?php
-                        $sql = "SELECT RoomType FROM roomtype";
-                        $result = mysqli_query($con, $sql);
-                        while ($row = mysqli_fetch_array($result)) {
-                            echo '<option value="' . $row['RoomType'] . '">' . $row['RoomType'] . '</option>';
-                        }
+                        // $sql = "SELECT RoomType FROM roomtype";
+                        // $result = mysqli_query($con, $sql);
+                        // while ($row = mysqli_fetch_array($result)) {
+                        //     echo '<option value="' . $row['RoomType'] . '">' . $row['RoomType'] . '</option>';
+                        // }
                         ?>
+                        <option value='STD'>Standard</option>
+                        <option value='SPR'>Superior</option>
+                        <option value='DLX'>Deluxe</option>
+                        <option value='SUT'>Suite</option>
+
                     </select>
                 </div>
             </div>
 
             <div class="form-row">
-                <div class="col">
+                <div class="col-sm-6">
                     <label for="validationPrice">Price</label>
-                    <input class="form-control is-invalid" id="validationRoomPrice" type="number" name="price" required onchange="validate()">
+                    <input class="form-control is-invalid" placeholder="Enter Price" id="validationRoomPrice" step="500" type="number" name="price" required min="0" onchange="pricevalidate()">
                 </div>
-                <div class="col">
+                <div class="col-sm-4">
                     <div class="form-group">
                         <label for="validationcurrency">Currency</label>
-                        <select name="currency" id="validationRoomCurrency" class="custom-select" required>
+                        <select name="currency" id="validationRoomCurrency" class="custom-select is-invalid" required onchange="currencyvalidate()">
                             <option selected value="">Choose currency</option>
                             <option value='thai'>Baht</option>
                             <option value='us'>Dollars</option>
@@ -65,67 +82,99 @@
 
                     </div>
                 </div>
-                <div class="col">
-                    <div class="col">
-                        <label for="validationCancel">Can be Cancel</label>
-                        <div class="custom-control custom-radio" id="validationRoomCancel">
-                            <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadio1">Yes</label>
-                        </div>
-                        <div class="custom-control custom-radio">
-                            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadio2">No</label>
-                        </div>
+
+                <div class="col-sm-2">
+                    <label for="validationCancel">Can be Cancel</label>
+                    <br>
+                    <div class="custom-control custom-radio custom-control-inline" id="validationRoomCancel">
+                        <input type="radio" id="customRadio1" name="cancel" class="custom-control-input" required value="Yes">
+                        <label class="custom-control-label" for="customRadio1">Yes</label>
+                    </div>
+                    <div class="custom-control custom-radio custom-control-inline" required>
+                        <input type="radio" id="customRadio2" name="cancel" class="custom-control-input" value="No">
+                        <label class="custom-control-label" for="customRadio2">No</label>
                     </div>
                 </div>
-                <br>
-                <button type="submit" class="btn btn-success">Select</button>
+            </div>
+
+            <div class="form-row">
+                <button type="submit" class="btn btn-success">Next</button>
+            </div>
 
         </form>
         <br>
-        <!-- <table class="table table-striped">
-        <thead class="thead-dark">
-            <tr>
-                <th>Position</th>
-                <th>Amount</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            // error_reporting(0);
-            // $Branch=$_GET['branch'];
-            // if($Branch!="")
-            // echo 'Branch : '.$Branch;
-            // $sql =
-            // "SELECT Position, COUNT(StaffID) AS Amount 
-            // FROM staffinfo
-            // WHERE BranchNo = '$Branch'
-            // GROUP BY Position";
-            // echo'<br>';
-            // $result = mysqli_query($con,$sql);
-            // $total = 0;
-            // while($row=mysqli_fetch_array($result)){
-            //     echo '<tr>';
-            //     echo '<td>'.$row['Position'].'</td>';
-            //     echo '<td>'.$row['Amount'].'</td>';
-            //     $total = $total + $row['Amount'];
-            //     echo '</tr>';
-            // }
-            // echo '<tr><td>Total</td><td>'.$total.'</td></tr>';
-            ?>
-           
 
-        </tbody>
-    </table> -->
+
+        <table class="table table-striped">
+            <thead class="thead-dark">
+                <tr>
+                    <th colspan="2">Detail</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $hotel = $_GET['branch'];
+                $roomnumber = $_GET['roomnumber'];
+                $roomtype = $_GET['roomtype'];
+                $price = $_GET['price'];
+                $currency = $_GET['currency'];
+                $canbecel = $_GET['cancel'];
+
+                echo '<tr><td>Staff Name</td><td>เดี๋ยวลิ้งเอา กูทำไม่เป็น</td>';
+                echo '<tr><td>Hotel</td><td>' . $hotel . '</td>';
+                echo '<tr><td>RoomID</td><td>' . $hotel . $roomtype . $roomnumber . '</td>';
+                echo '<tr><td>Price</td><td>' . $price . '</td>';
+                echo '<tr><td>Can Be Cancel</td><td>' . $canbecel . '</td>';
+
+                ?>
+
+
+            </tbody>
+        </table>
+
+
     </div>
     <script>
-        function validate() {
+        function pricevalidate() {
             if (document.getElementById("validationRoomPrice").value) {
                 document.getElementById("validationRoomPrice").className = "form-control is-valid";
-            }
-            else {
+            } else {
                 document.getElementById("validationRoomPrice").className = "form-control is-invalid";
             }
+        }
+
+        function roomnumbervalidate() {
+            if (document.getElementById("validationRoomNumber").value) {
+                document.getElementById("validationRoomNumber").className = "form-control is-valid";
+            } else {
+                document.getElementById("validationRoomNumber").className = "form-control is-invalid";
+            }
+        }
+
+        function hotelvalidate() {
+            if (document.getElementById("validationHotel").value) {
+                document.getElementById("validationHotel").className = "custom-select is-valid";
+            } else {
+                document.getElementById("validationHotel").className = "custom-select is-invalid";
+            }
+        }
+
+        function roomtypevalidate() {
+            if (document.getElementById("validationRoomType").value) {
+                document.getElementById("validationRoomType").className = "custom-select is-valid";
+            } else {
+                document.getElementById("validationRoomType").className = "custom-select is-invalid";
+            }
+
+        }
+
+        function currencyvalidate() {
+            if (document.getElementById("validationRoomCurrency").value) {
+                document.getElementById("validationRoomCurrency").className = "custom-select is-valid";
+            } else {
+                document.getElementById("validationRoomCurrency").className = "custom-select is-invalid";
+            }
+
         }
     </script>
 </body>
