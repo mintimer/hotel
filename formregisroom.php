dc80fb5a263a972510b40de096c8068224710db5
manmanandman <prakasit_sw2@outlook.com>
2019-05-26 15:46:41 +0700

    formman

--- formregisroom.php
+++ formregisroom.php
@@ -7,6 +7,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <?php include('connect.php');
+    $ticket = 1;
     ?>
 </head>
 
@@ -15,11 +16,21 @@
         <h3>Room Registration</h3>
         ฟอร์มเพิ่มห้องพัก <br><br>
 
-        <form action="#" method="get">
+        <form id="form1" action="#" method="get">
             <label for="validationHotel">Select Hotel</label>
-            <select name="branch" id="validationHotel" class="custom-select" required>
-                <option selected value="">Hotel</option>
+            <select name="branch" id="validationHotel" class="custom-select is-invalid" required onchange="hotelvalidate()">
+
                 <?php
+                if (isset($_GET['branch'])) {
+                    $temp = $_GET['branch'];
+                    $sql = "SELECT branchname FROM branchinfo WHERE branchno = '$temp'";
+                    $result = mysqli_query($con, $sql);
+                    $row = mysqli_fetch_array($result);
+
+
+
+                    echo '<option selected value="' . $_GET['branch'] . '">' . $row['branchname'] . '</option>';
+                } else echo '<option selected value="">Hotel</option>';
                 $sql = "SELECT branchname,branchno FROM branchinfo";
                 $result = mysqli_query($con, $sql);
                 while ($row = mysqli_fetch_array($result)) {
@@ -36,15 +47,18 @@
                 </div>
                 <div class="col">
                     <label for="validationRoomType">Room Type</label>
-                    <select name="roomtype" id="validationRoomType" class="custom-select" required>
-                        <option selected value="">Choose Room Type</option>
+                    <select name="roomtype" id="validationRoomType" class="custom-select is-invalid" required onchange="roomtypevalidate()">
                         <?php
-                        $sql = "SELECT RoomType FROM roomtype";
-                        $result = mysqli_query($con, $sql);
-                        while ($row = mysqli_fetch_array($result)) {
-                            echo '<option value="' . $row['RoomType'] . '">' . $row['RoomType'] . '</option>';
-                        }
+                        if (isset($_GET['roomtype'])) {
+                            echo '<option selected value="' . $_GET['roomtype'] . '">' . $_GET['roomtype'] . '</option>';
+                        } else echo '<option selected value="">Choose Room Type</option>';
+
                         ?>
+                        <option value='Standard'>Standard</option>
+                        <option value='Superior'>Superior</option>
+                        <option value='Deluxe'>Deluxe</option>
+                        <option value='Suite'>Suite</option>
+
                     </select>
                 </div>
             </div>
@@ -52,80 +66,227 @@
             <div class="form-row">
                 <div class="col">
                     <label for="validationPrice">Price</label>
-                    <input class="form-control is-invalid" id="validationRoomPrice" type="number" name="price" required onchange="validate()">
+                    <input class="form-control is-invalid" placeholder="Enter Price" id="validationRoomPrice" step="500" type="number" name="price" required min="0" onchange="pricevalidate()" value="<?php if (isset($_GET['price'])) echo $_GET['price']; ?>">
                 </div>
                 <div class="col">
                     <div class="form-group">
                         <label for="validationcurrency">Currency</label>
-                        <select name="currency" id="validationRoomCurrency" class="custom-select" required>
-                            <option selected value="">Choose currency</option>
-                            <option value='thai'>Baht</option>
-                            <option value='us'>Dollars</option>
+                        <select name="currency" id="validationRoomCurrency" class="custom-select is-invalid" required onchange="currencyvalidate()">
+                            <?php
+                            if (isset($_GET['currency'])) {
+                                $temp = $_GET['currency'];
+
+                                echo '<option selected value="' . $_GET['currency'] . '">' . $_GET['currency'] . '</option>';
+                            } else echo '<option selected value="">Choose currency</option>';
+                            ?>
+                            <option value='THA'>Baht</option>
+                            <option value='USD'>Dollars</option>
                         </select>
 
                     </div>
                 </div>
-                <div class="col">
-                    <div class="col">
-                        <label for="validationCancel">Can be Cancel</label>
-                        <div class="custom-control custom-radio" id="validationRoomCancel">
-                            <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadio1">Yes</label>
-                        </div>
-                        <div class="custom-control custom-radio">
-                            <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
-                            <label class="custom-control-label" for="customRadio2">No</label>
-                        </div>
+
+                <div class="col-sm-2">
+                    <label for="validationCancel">Can be Cancel</label>
+                    <br>
+                    <?php
+                    if (isset($_GET['cancel'])) {
+                        if ($_GET['cancel'] == 'Yes')
+                            echo '<div class="custom-control custom-radio custom-control-inline" id="validationRoomCancel">
+                        <input type="radio" id="customRadio1" name="cancel" class="custom-control-input" checked value="Yes">
+                        <label class="custom-control-label" for="customRadio1">Yes</label>
                     </div>
+                    <div class="custom-control custom-radio custom-control-inline" required>
+                        <input type="radio" id="customRadio2" name="cancel" class="custom-control-input" value="No">
+                        <label class="custom-control-label" for="customRadio2">No</label>
+                    </div>';
+                        else
+                            echo '<div class="custom-control custom-radio custom-control-inline" id="validationRoomCancel">
+                        <input type="radio" id="customRadio1" name="cancel" class="custom-control-input"  value="Yes">
+                        <label class="custom-control-label" for="customRadio1">Yes</label>
+                    </div>
+                    <div class="custom-control custom-radio custom-control-inline" required>
+                        <input type="radio" id="customRadio2" name="cancel" class="custom-control-input" checked value="No">
+                        <label class="custom-control-label" for="customRadio2">No</label>
+                    </div>';
+                    } else {
+                        echo '<div class="custom-control custom-radio custom-control-inline" id="validationRoomCancel">
+                    <input type="radio" id="customRadio1" name="cancel" class="custom-control-input" required value="Yes">
+                    <label class="custom-control-label" for="customRadio1">Yes</label>
                 </div>
-                <br>
-                <button type="submit" class="btn btn-success">Select</button>
+                <div class="custom-control custom-radio custom-control-inline" required>
+                    <input type="radio" id="customRadio2" name="cancel" class="custom-control-input" value="No">
+                    <label class="custom-control-label" for="customRadio2">No</label>
+                </div>';
+                    }
+
+
+                    ?>
+
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md">
+                    <button type="submit" class="btn btn-primary">Next</button>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <br>
+            <?php
+            if ($ticket == 1) {
+
+                echo '<table class="invisible" id="table">
+              <thead class="thead-dark">
+                  <tr>
+                      <th colspan="2">Detail</th>
+                  </tr>
+              </thead>
+              <tbody>';
+                error_reporting(0);
+                $hotel = $_GET['branch'];
+                $sql = "SELECT BranchName FROM branchinfo WHERE BranchNo = '$hotel'";
+                $result = mysqli_query($con, $sql);
+                $row = mysqli_fetch_array($result);
+                $roomnumber = $_GET['roomnumber'];
+
+                $temp1 = $_GET['roomtype'];
+                if ($temp1 == 'Standard') $roomtype = 'STD';
+                else if ($temp1 == 'Deluxe') $roomtype = 'DLX';
+                else if ($temp1 == 'Suite') $roomtype = 'SUT';
+                else if ($temp1 == 'Superior') $roomtype = 'SPR';
+
+
+
+
+                $currency = $_GET['currency'];
+                if ($currency == "USD") {
+                    $price = $_GET['price'] * 30;
+                } else {
+                    $price = $_GET['price'];
+                }
+                $canbecel = $_GET['cancel'];
+
+                if (!$roomtype) {
+                    $ticket = 1;
+                } else {
+                    $ticket = 2;
+                }
+
+                echo '<tr><td>Staff Name</td><td>เดี๋ยวลิ้งเอา กูทำไม่เป็น</td>';
+                echo '<tr><td>Hotel</td><td>' . $row['BranchName'] . '</td>';
+                echo '<tr><td>RoomID</td><td>' . $hotel . $roomtype . $roomnumber . '</td>';
+                echo '<tr><td>Price in Baht</td><td>' . $price . '</td>';
+                echo '<tr><td>Can Be Cancel</td><td>' . $canbecel . '</td>';
+                echo ' </tbody> </table>';
+            }
+
+
+            ?>
+
+
+
+
 
         </form>
+
+        <?php
+        if ($ticket == 2) {
+            echo
+                '<div class="row">
+                    <div class="col-md">
+                <button type="submit" class="btn btn-success" onclick="sendfunction()">Submit</button>
+                </div>
+                </div>';
+        }
+
+        ?>
+        <p id="sent"></p>
         <br>
-        <!-- <table class="table table-striped">
-        <thead class="thead-dark">
-            <tr>
-                <th>Position</th>
-                <th>Amount</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            // error_reporting(0);
-            // $Branch=$_GET['branch'];
-            // if($Branch!="")
-            // echo 'Branch : '.$Branch;
-            // $sql =
-            // "SELECT Position, COUNT(StaffID) AS Amount 
-            // FROM staffinfo
-            // WHERE BranchNo = '$Branch'
-            // GROUP BY Position";
-            // echo'<br>';
-            // $result = mysqli_query($con,$sql);
-            // $total = 0;
-            // while($row=mysqli_fetch_array($result)){
-            //     echo '<tr>';
-            //     echo '<td>'.$row['Position'].'</td>';
-            //     echo '<td>'.$row['Amount'].'</td>';
-            //     $total = $total + $row['Amount'];
-            //     echo '</tr>';
-            // }
-            // echo '<tr><td>Total</td><td>'.$total.'</td></tr>';
-            ?>
-           
+
+
+
+
+
+
+
 
         </tbody>
-    </table> -->
+    </table> 
     </div>
     <script>
-        function validate() {
+        <?php
+        if (isset($_GET['roomnumber']))
+            echo 'document.getElementById("validationRoomNumber").className = "form-control is-valid";
+            document.getElementById("table").className = "table table-striped";';
+
+        if (isset($_GET['price']))
+            echo 'document.getElementById("validationRoomPrice").className = "form-control is-valid";';
+
+        if (isset($_GET['branch']))
+            echo 'document.getElementById("validationHotel").className = "custom-select is-valid";';
+
+        if (isset($_GET['currency']))
+            echo 'document.getElementById("validationRoomCurrency").className = "custom-select is-valid";';
+
+        if (isset($_GET['roomtype']))
+            echo 'document.getElementById("validationRoomType").className = "custom-select is-valid";';
+        ?>
+
+        function pricevalidate() {
             if (document.getElementById("validationRoomPrice").value) {
                 document.getElementById("validationRoomPrice").className = "form-control is-valid";
             }
             else {
                 document.getElementById("validationRoomPrice").className = "form-control is-invalid";
             }
+        }
+
+        function sendfunction() {
+
+            document.getElementById("sent").innerHTML =
+                "<?php
+                    include('connect.php');
+                    // echo $hotel."   ";
+                    // $id =  $hotel . $roomtype . $roomnumber;
+                    // echo $id."   ";
+                    // echo $price."   ";
+                    // echo $canbecel."   ";
+                     $sql = "INSERT INTO roominfo (RoomID, BranchNo, RoomType, CanBeCancel, RoomPrice)
+                     VALUES ('$id', '$hotel', '$temp1', '$canbecel', '$price')";
+                    // if (!mysqli_query($con,$sql)) {
+                    //     echo "ERROR";
+                    //     }
+                    //     else echo "1 record added";
+                    // mysqli_close($con);
+                    echo 'add';
+                    ?>";
+
+
+
+
+
+
+
+
+
         }
     </script>
 </body>
