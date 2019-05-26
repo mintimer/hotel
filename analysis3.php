bc9c9f45c18275134c7a374e44c400877e590543
manmanandman <prakasit_sw2@outlook.com>
2019-05-26 16:55:33 +0700

    ใส่กรอบตารางเหมือนเมื่อเช้า

--- analysis3.php
+++ analysis3.php
@@ -9,6 +9,17 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <?php include('connect.php');?>
         <link rel="stylesheet" href="bgstaffreport.css">
+        <style>
+        table,
+        th,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+        }
+        th {
+            text-align: center;
+        }
+    </style>
     </head>
 
     <body class="bgstaffreport"> 
