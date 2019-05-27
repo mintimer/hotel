a751b98b0adc702333ea4a264a527fb6a507a551
phuraefa <justfang.pv@gmail.com>
2019-05-27 20:51:48 +0700

    เปลี่ยนพื้นหลัง regisroom

--- formregisroom.php
+++ formregisroom.php
@@ -9,7 +9,7 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <?php include('connect.php');
         $ticket = 1;?>
-        <link rel="stylesheet" href="bgstaffreport.css">
+        <link rel="stylesheet" href="bgregisroom.css">
         <style>
         table,
         th,
@@ -23,7 +23,7 @@
     </style>
     </head>
 
-    <body class="bgstaffreport"> 
+    <body class="bgregisroom"> 
         <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #339999;" id="mynav">
             <a class="navbar-brand" style="color: white">
                 <img src="pic/logo.png" width="30" height="30" class="d-inline-block align-top" alt="">
